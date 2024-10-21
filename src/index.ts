a6caec9b72817fbf2d12f752234a43b0e9b6cc0d
Gabriel Rufino <contato@gabrielrufino.com>
2024-10-21 19:04:52 -0300

    feat: exposing classes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 export * from './Math/Fibonacci';
+export * from './Math/LinearRegression';
+export * from './Math/TwoSum';
+
+export * from './Search/BinarySearch'
+export * from './Search/LinearSearch'
 
 export * from './Sort/BubbleSort';
 export * from './Sort/SelectionSort';
