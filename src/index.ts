c3f734c3b4f0570862cb32a7243353df19fd73c4
Gabriel Rufino <contato@gabrielrufino.com>
2024-10-21 19:11:31 -0300

    fix: lint

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ export * from './Math/Fibonacci';
 export * from './Math/LinearRegression';
 export * from './Math/TwoSum';
 
-export * from './Search/BinarySearch'
-export * from './Search/LinearSearch'
+export * from './Search/BinarySearch';
+export * from './Search/LinearSearch';
 
 export * from './Sort/BubbleSort';
 export * from './Sort/SelectionSort';
