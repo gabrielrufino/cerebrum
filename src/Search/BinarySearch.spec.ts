ca78765aa972430ecd97164af19421e475baaeb6
Gabriel Rufino <contato@gabrielrufino.com>
2025-02-11 05:16:46 -0300

    fix: removing tests file from build

--- src/Search/BinarySearch.spec.ts
+++ src/Search/BinarySearch.spec.ts
@@ -1,6 +1,6 @@
 import { describe } from 'vitest'
 import { BinarySearch } from './BinarySearch'
-import { SearchTestsSuite } from './Search.test'
+import { SearchTestsSuite } from './Search.spec'
 
 describe('BinarySearch', () => {
   SearchTestsSuite.execute(BinarySearch)
